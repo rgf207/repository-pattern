24cebec3a0b81acafbc6cb595e4cf875a3fa5bf7
Robb Fountain <robb@131studios.com>
2018-04-20 10:30:08 -0400

    added tests

---
+++ tests/RepositoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class RepositoryTest extends TestCase
+{
+
+    /** @test */
+    public function it_creates_a_repository_directory_and_repository_class ()
+    {
+        $this->assertFalse(is_dir(app_path('Repositories')));
+
+        Artisan::call('onethirtyone:make-repository', ['name' => 'otoTest']);
+
+        $this->assertTrue(is_dir(app_path('Repositories')));
+        $this->assertTrue(is_file(app_path('Repositories/otoTestRepository.php')));
+
+        unlink(app_path('Repositories/otoTestRepository.php'));
+        if (scandir(app_path('Repositories')) == 2) {
+            rmdir(app_path('Repositories'));
+        }
+    }
+
+    /** @test */
+    public function it_creates_a_model_when_the_option_is_set ()
+    {
+        $this->assertFalse(is_file(app_path('otoTest.php')));
+
+        Artisan::call('onethirtyone:make-repository', ['name' => 'otoTest', '-m' => true]);
+
+        $this->assertTrue(is_file(app_path('otoTest.php')));
+
+        unlink(app_path('Repositories/otoTestRepository.php'));
+        unlink(app_path('otoTest.php'));
+        if (scandir(app_path('Repositories')) == 2) {
+            rmdir(app_path('Repositories'));
+        }
+    }
+
+
+}
